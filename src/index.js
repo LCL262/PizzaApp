686281f347f6fb86ad933dfeb448808860180e3b
LCL262 <24037880@myrp.edu.sg>
2025-10-24 12:34:51 +0800

    Initial commit: add React pizza app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function PizzaPic(){
+    return (
+        <img
+        src="/pizzas/Spinach.jpg"
+        alt="Spinach Pizza"
+        height="400"
+        width="500"
+        />
+    );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function PizzaName(){
+    return <h2>Spinach Pizza</h2>
+};
+
+function PizzaInfo(){
+    return <text>Tomato, mozarella, spinach and ricotta cheese.</text>
+};
+
+function Price(){
+    return <text>10</text>
+};
+
+function App(){
+    return (
+    <div>
+        <h1>Andy's Pizza Co.</h1>
+        <PizzaPic />
+        <PizzaName />
+        <PizzaInfo />
+        <div></div>
+        <Price />
+    </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);

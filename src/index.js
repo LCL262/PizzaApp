7b0d871f62a79fd8daa3cf5c171c183f56385c6b
LCL262 <24037880@myrp.edu.sg>
2025-11-10 12:39:13 +0800

    1

--- src/index.js
+++ src/index.js
@@ -4,54 +4,68 @@ import ReactDOM from "react-dom/client";
 function PizzaList({ name, image, description, price }) {
   return (
     <div>
-      <img src={image} alt={name} width="500" height="400" />
+      <img src={image} alt={name}/>
       <h2>{name}</h2>
       <p>{description}</p>
       <p>${price}</p>
     </div>
   );
-}
+};
 
+function Header() {
+  return <h1 style={{color: "orange", fontSize: "48px", textTransform: "uppercase" }}>Chunlin's Pizza Co.</h1>;
+};
 
+function Menu() {
+    const pizzas = [
+  {
+    name: "Spinach Pizza",
+    image: "/pizzas/Spinach.jpg",
+    description: "Tomato, mozzarella, spinach, and ricotta cheese.",
+    price: 10
+  },
+  {
+    name: "Pepperoni Pizza",
+    image: "/pizzas/Pepperoni.jpg",
+    description: "Tomato, mozzarella, and pepperoni slices.",
+    price: 12
+  },
+  {
+    name: "Margherita Pizza",
+    image: "/pizzas/Margherita.jpg",
+    description: "Tomato, mozzarella, fresh basil, and olive oil.",
+    price: 9
+  }
+  ];
+  return (
+  <div className="menu">
+    <h2>Our Menu</h2>
+    {pizzas.map((pizza, index) => (
+      <PizzaList
+        key={index}
+        name={pizza.name} 
+        description={pizza.description}
+        price={pizza.price}
+        image={pizza.image}
+        />
+    ))}
+  </div>
+  );
+};
+
+function Footer(){
+  return <footer className="footer">We're currently open</footer>
+};
 
 function App() {
-  // You can add more pizza objects here
-  const pizzas = [
-    {
-      name: "Spinach Pizza",
-      image: "/pizzas/Spinach.jpg",
-      description: "Tomato, mozzarella, spinach, and ricotta cheese.",
-      price: 10
-    },
-    {
-      name: "Pepperoni Pizza",
-      image: "/pizzas/Pepperoni.jpg",
-      description: "Tomato, mozzarella, and pepperoni slices.",
-      price: 12
-    },
-    {
-      name: "Margherita Pizza",
-      image: "/pizzas/Margherita.jpg",
-      description: "Tomato, mozzarella, fresh basil, and olive oil.",
-      price: 9
-    }
-  ];
-
   return (
-    <div>
-      <h1>Chunlin's Pizza Co.</h1>
-      {pizzas.map((pizza, index) => (
-        <PizzaList
-          key={index}
-          name={pizza.name}
-          image={pizza.image}
-          description={pizza.description}
-          price={pizza.price}
-        />
-      ))}
+    <div className="container">
+      <Header/>
+      <Menu/>
+      <Footer/>
     </div>
   );
-}
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
